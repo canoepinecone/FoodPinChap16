6e6edffcaefbc826801a7633b4d7ad206a1f11d6
Melinda Swartzfager <melinda@melindas-macbook-pro.local>
2018-02-22 11:25:55 -0800

    Chapter 19 Exercise #1: Fix restaurant description display bug. (Switch to RestaurantMO's summary object instead of description)

--- FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -89,7 +89,7 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             return cell
         case 2:
             let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
-            cell.descriptionLabel.text = restaurant.description
+            cell.descriptionLabel.text = restaurant.summary
             
             return cell
         case 3:
