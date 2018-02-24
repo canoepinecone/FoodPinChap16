ced716545dc9258fe055e122026fbcd19858dcf1
Melinda Swartzfager <melinda@melindas-macbook-pro.local>
2018-02-24 00:28:25 -0800

    More work on walkthrough intro.

---
+++ FoodPin/Controller/WalkthroughContentViewController.swift
@@ -0,0 +1,60 @@
+//
+//  WalkthroughContentViewController.swift
+//  FoodPin
+//
+//  Created by Melinda Swartzfager on 2/23/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughContentViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet var headingLabel: UILabel! {
+        didSet {
+            headingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var subHeadingLabel: UILabel! {
+        didSet {
+            subHeadingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var contentImageView: UIImageView!
+
+    // MARK: - Properties
+    
+    var index = 0
+    var heading = ""
+    var subHeading = ""
+    var imageFile = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        headingLabel.text = heading
+        subHeadingLabel.text = subHeading
+        contentImageView.image = UIImage(named: imageFile)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
