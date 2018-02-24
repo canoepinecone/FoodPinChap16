7b08c706a86fdf3501248754d465d47029c5d065
Melinda Swartzfager <melinda@melindas-macbook-pro.local>
2018-02-24 01:45:18 -0800

    More work on chapter 21

---
+++ FoodPin/Controller/WalkthroughPageViewController.swift
@@ -0,0 +1,70 @@
+//
+//  WalkthroughPageViewController.swift
+//  FoodPin
+//
+//  Created by Melinda Swartzfager on 2/24/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughPageViewController: UIPageViewController, UIPageViewControllerDataSource {
+    
+    var pageHeadings = ["CREATE YOUR OWN FOOD GUIDE", "SHOW YOU THE LOCATION", "DISCOVER GREAT RESTAURANTS"]
+    var pageImages = ["onboarding-1", "onboarding-2", "onboarding-3"]
+    var pageSubHeadings = ["Pin your favorite restaurants and create your own food guide", "Search and locate your favorite restaurants on maps", "Find restaurants shared by your friends and other foodies"]
+    
+    var currentIndex = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dataSource = self
+        
+        if let startingViewController = contentViewController(at: 0) {
+            setViewControllers([startingViewController], direction: .forward, animated: true, completion: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        var index = (viewController as! WalkthroughContentViewController).index
+        index -= 1
+        
+        return contentViewController(at: index)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        var index = (viewController as! WalkthroughContentViewController).index
+        index += 1
+        
+        return contentViewController(at: index)
+    }
+    
+    func contentViewController(at index: Int) -> WalkthroughContentViewController? {
+        
+        if index < 0 || index >= pageHeadings.count {
+            return nil
+        }
+        
+        let storyBoard = UIStoryboard(name: "Onboarding", bundle: nil)
+        if let pageContentViewController = storyBoard.instantiateViewController(withIdentifier: "WalkthroughContentViewController") as? WalkthroughContentViewController {
+            
+            pageContentViewController.imageFile = pageImages[index]
+            pageContentViewController.heading = pageHeadings[index]
+            pageContentViewController.subHeading = pageSubHeadings[index]
+            pageContentViewController.index = index
+            
+            return pageContentViewController
+        }
+        
+        return nil
+    }
+
+}
